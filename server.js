48e2f9cc912bf0f85e48b1ee21138d766ab981e4
amatos <amatos201@gmail.com>
2022-08-04 16:58:36 -0400

    starter code

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { notes } = require('./data/notes.json');
+const apiRoutes = require('./routes/apiRoutess');
+const htmlRoutes = require('./routes/htmlRoutess');
+const app = express();
+const PORT= process.env.PORT || 3001;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port 3001!`);
+  });
