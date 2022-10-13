434d81729c85186fac6e1bb9439d4e4025a0fdae
amatos <amatos201@gmail.com>
2022-10-13 11:51:00 -0400

    changes to server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { notes } = require('./data/notes.json');
+
 const apiRoutes = require('./routes/apiRoutess');
 const htmlRoutes = require('./routes/htmlRoutess');
 const app = express();
